1e410a63e1d280a137f6ef929f80de23adce6663
sangram2200 <sangramshinde2000@gmail.com>
2022-06-21 17:55:24 +0530

    error success messages

--- 18- Forkify/src/js/controller.js
+++ 18- Forkify/src/js/controller.js
@@ -24,7 +24,7 @@ const controlRecipes = async function () {
     //2) rendering the recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
